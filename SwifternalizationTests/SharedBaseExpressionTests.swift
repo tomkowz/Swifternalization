c00f07941ce930b20c58b7c59cb4e5a472c17419
Tomasz Szulc <mail@szulctomasz.com>
2015-06-28 20:46:50 +0200

    Move shared expression classes to separated directory and add missing tests.

---
+++ SwifternalizationTests/SharedBaseExpressionTests.swift
@@ -0,0 +1,49 @@
+//
+//  SharedBaseExpressionTests.swift
+//  Swifternalization
+//
+//  Created by Tomasz Szulc on 28/06/15.
+//  Copyright (c) 2015 Tomasz Szulc. All rights reserved.
+//
+
+import UIKit
+import XCTest
+import Swifternalization
+
+class SharedBaseExpressionTests: XCTestCase {
+
+    func testOne() {
+        let sharedExp = SharedBaseExpression.allExpressions().filter({$0.key == "one"}).first!
+        let expression = Expression(pattern: sharedExp.expression)
+        
+        XCTAssertTrue(expression.validate("1"), "Should match 1")
+        XCTAssertFalse(expression.validate("2"), "Should not match 2")
+    }
+    
+    func testMoreThanOne() {
+        let sharedExp = SharedBaseExpression.allExpressions().filter({$0.key == ">one"}).first!
+        let expression = Expression(pattern: sharedExp.expression)
+        
+        XCTAssertTrue(expression.validate("2"), "Should match 2")
+        XCTAssertTrue(expression.validate("3"), "Should match 3")
+        XCTAssertFalse(expression.validate("1"), "Should not match 1")
+    }
+    
+    func testTwo() {
+        let sharedExp = SharedBaseExpression.allExpressions().filter({$0.key == "two"}).first!
+        let expression = Expression(pattern: sharedExp.expression)
+        
+        XCTAssertTrue(expression.validate("2"), "Should match 2")
+        XCTAssertFalse(expression.validate("1"), "Should not match 1")
+    }
+    
+    func testOther() {
+        let sharedExp = SharedBaseExpression.allExpressions().filter({$0.key == "other"}).first!
+        let expression = Expression(pattern: sharedExp.expression)
+        
+        XCTAssertTrue(expression.validate("0"), "Should match 0")
+        XCTAssertTrue(expression.validate("2"), "Should match 2")
+        XCTAssertTrue(expression.validate("3"), "Should match 3")
+        XCTAssertFalse(expression.validate("1"), "Should not match 1")
+    }
+}
---
+++ SwifternalizationTests/SharedPolishExpressionTests.swift
@@ -0,0 +1,37 @@
+//
+//  SharedPolishExpressionTests.swift
+//  Swifternalization
+//
+//  Created by Tomasz Szulc on 28/06/15.
+//  Copyright (c) 2015 Tomasz Szulc. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class SharedPolishExpressionTests: XCTestCase {
+
+    func testFew() {
+        let sharedExp = SharedPolishExpression.allExpressions().filter({$0.key == "few"}).first!
+        let expression = Expression(pattern: sharedExp.expression)
+        
+        XCTAssertTrue(expression.validate("2"), "Should match 2")
+        XCTAssertTrue(expression.validate("24"), "Should match 24")
+        XCTAssertTrue(expression.validate("153"), "Should match 153")
+
+        XCTAssertFalse(expression.validate("12"), "Should not match 12")
+        XCTAssertFalse(expression.validate("25"), "Should not match 25")
+    }
+    
+    func testMany() {
+        let sharedExp = SharedPolishExpression.allExpressions().filter({$0.key == "many"}).first!
+        let expression = Expression(pattern: sharedExp.expression)
+        
+        XCTAssertTrue(expression.validate("10"), "Should match 10")
+        XCTAssertTrue(expression.validate("18"), "Should match 18")
+        XCTAssertTrue(expression.validate("1009"), "Should match 1009")
+        
+        XCTAssertFalse(expression.validate("22"), "Should not match 22")
+        XCTAssertFalse(expression.validate("24"), "Should not match 24")
+    }
+}
